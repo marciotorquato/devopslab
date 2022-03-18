5552dec8cb88a1865c5bcc62d31482f436259424
marciotorquato <marcio.limat@gmail.com>
2022-03-17 23:19:12 -0300

    teste 4

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from app import app
 import unittest
 
 class Test(unittest.TestCase):
+
     def setUp(self):
         # cria uma instância do unittest, precisa do nome "setUp"
         self.app = app.test_client()
@@ -13,7 +14,7 @@ class Test(unittest.TestCase):
     def test_requisicao(self):
         # compara o status da requisicao (precisa ser igual a 200)
         self.assertEqual(self.result.status_code, 200)
-
+        
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
         self.assertEqual(self.result.data.decode('utf-8'), "Hello World - Marcio Torquato")
