a98633060942ffa93ac56391915d78c3bbedc5f0
marciotorquato <marcio.limat@gmail.com>
2022-03-17 23:22:30 -0300

    correcao final

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask
-from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
-
-csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
     return "Hello World - Marcio Torquato"
-    
+
 if __name__ == '__main__':
     app.run()
