6f1a0933f85b7b1cd1bb6bdcbcef9d05d53aad4e
marciotorquato <marcio.limat@gmail.com>
2022-03-17 22:47:36 -0300

    correcao hotspot

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
-csrf = CSRFProtect()
-csrf.init_app(app) # Compliant
+app.config['WTF_CSRF_ENABLED'] = False
 
 @app.route("/")
 def pagina_inicial():
