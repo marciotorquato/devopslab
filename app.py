8f9cc7d307e0bf79d03531d8b2ea8c997de8f707
marciotorquato <marcio.limat@gmail.com>
2022-03-20 14:56:46 -0300

    teste

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - Marcio Torquato"
+    return "Hello World - Marcio Torquato Deploy Heroku"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
