0c7a66b84c75b60016c911a8733f21465a7e1258
marciotorquato <marcio.limat@gmail.com>
2022-03-28 18:58:44 -0300

    Alterando texto home

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - Marcio Torquato"
+    return "Hello World - Grupo 14 - v.1.0"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
